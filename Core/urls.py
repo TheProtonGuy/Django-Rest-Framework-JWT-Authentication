90b4334e0ec686f8220374fedf8c09d4e6733e33
Ayodeji Adesola <95865361+TheProtonGuy@users.noreply.github.com>
2024-12-31 12:38:39 +0100

    First Commit

---
+++ Core/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('', views.Home),
+    path('api/token/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('my-protected-endpoint/', views.MyProtectedRoute),
+]
